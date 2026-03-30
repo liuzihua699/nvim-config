d87b67ef8f46e5447e3b5bba0668f3b80b4de8cf
liuzihua <2274298503@qq.com>
2026-03-30 17:25:18 +0800

    添加默认剪切板支持

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,3 +12,22 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
+
+
+-- 让 y/d/p 默认使用系统剪贴板
+vim.opt.clipboard = "unnamedplus"
+
+-- 使用 OSC 52 作为剪贴板 provider（远程服务器必需）
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
+
